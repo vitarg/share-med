10f4e433a26626d0d5194646f8e5d70ca2653b7d
vitarg <vitrigovm@gmail.com>
2021-09-06 14:14:51 +0300

    добавил новые роуты в индексный роут

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.use(require("./categories.route"));
+router.use(require("./requests.route"));
+router.use(require("./medications.route"));
 
 module.exports = router;
