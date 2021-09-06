22a46a2987786caea95ba243891e0377d8115860
Adam <bisliev_adam@mail.ru>
2021-09-06 13:59:45 +0300

    Добавил модель,роут и контроллер для лекарств

---
+++ models/Medication.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const medicationSchema = mongoose.Schema({
+  name: String,
+  price: Number,
+  descr: String,
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  img: String,
+  hasRecipe: Boolean,
+  expiryDate: Number,
+});
+
+const Medication = mongoose.model("Medication", medicationSchema);
+
+module.exports = Medication;
