fa2184f921249a881e440c55932de2184e231900
vitarg <vitrigovm@gmail.com>
2021-09-11 14:24:36 +0300

    исправил конфликты и поправил некоторые моменты

--- client/src/components/pages/MainPage.jsx
+++ client/src/components/pages/MainPage.jsx
@@ -9,12 +9,13 @@ import {
   CardActionArea,
   CardActions,
   CardContent,
-  CardMedia, CircularProgress,
-  Grid, TextField,
-  Typography
+  CardMedia,
+  CircularProgress,
+  Grid,
+  TextField,
+  Typography,
 } from "@material-ui/core";
 import DialogForm from "./DialogForm";
-import Loading from '../Loading';
 
 const MainPage = () => {
   const dispatch = useDispatch();
@@ -25,45 +26,102 @@ const MainPage = () => {
     dispatch(getMedications());
   }, []);
 
-
   const handleClickOpen = () => {
     setOpen(true);
   };
 
-  const { medications } = useSelector((state) => state.medications);
-  const token = useSelector((state) => state.application.token);
-
-
   const { id } = useParams();
 
   const { medications, loading } = useSelector((state) => state.medications);
+  const token = useSelector((state) => state.application.token);
 
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("");
   const some = (e) => {
-    setSearch(e.target.value)
-  }
+    setSearch(e.target.value);
+  };
 
   if (loading) {
-    return (<div>
+    return (
+      <div>
         <CircularProgress />
-    </div>
+      </div>
     );
   }
 
-
   if (id) {
-    return (<>
-          <TextField
-            id="standard-search"
-            label="Search field"
-            type="search"
-            value={search}
-            onChange={some}
-          />
-        <Grid container justifyContent={"space-around"} >
+    return (
+      <>
+        <TextField
+          id="standard-search"
+          label="Search field"
+          type="search"
+          value={search}
+          onChange={some}
+        />
+        <Grid container justifyContent={"space-around"}>
           {medications
-          .filter((item) => item.category === id)
-          .filter(item => search === "" ? item : item.name.toLowerCase().includes(search))
+            .filter((item) => item.category === id)
+            .filter((item) =>
+              search === "" ? item : item.name.toLowerCase().includes(search)
+            )
+            .map((item) => {
+              return (
+                <Grid item xs={3} key={item._id}>
+                  <Card>
+                    <CardActionArea>
+                      <CardMedia
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        title="Contemplative Reptile"
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          {item.name}
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color="textSecondary"
+                          component="p"
+                        >
+                          {item.descr}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                      <Button size="small" color="primary">
+                        <Link to={`/medications/${item._id}`}>Подробнее</Link>
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              );
+            })}
+        </Grid>
+      </>
+    );
+  }
+  return (
+    <>
+      {token ? (
+        <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+          Добавить +
+        </Button>
+      ) : (
+        ""
+      )}
+      <DialogForm setOpen={setOpen} open={open} />
+
+      <TextField
+        id="standard-search"
+        label="Search field"
+        type="search"
+        onChange={some}
+      />
+
+      <Grid container justifyContent={"space-around"}>
+        {medications
+          .filter((item) =>
+            search === "" ? item : item.name.toLowerCase().includes(search)
+          )
           .map((item) => {
             return (
               <Grid item xs={3} key={item._id}>
@@ -77,8 +135,16 @@ const MainPage = () => {
                       <Typography gutterBottom variant="h5" component="h2">
                         {item.name}
                       </Typography>
+
                       <Typography
-                        variant="body2"
+                        variant="h6"
+                        color="textSecondary"
+                        component="p"
+                      >
+                        {item.price ? item.price : "Бесплатно"}
+                      </Typography>
+                      <Typography
+                        variant="body1"
                         color="textSecondary"
                         component="p"
                       >
@@ -95,80 +161,6 @@ const MainPage = () => {
               </Grid>
             );
           })}
-        </Grid>
-    </>
-
-    );
-  }
-  return (
-    <>
-
-      {token ? (
-        <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-          Добавить +
-        </Button>
-      ) : (
-        ""
-      )}
-      <DialogForm setOpen={setOpen} open={open} />
-      <Grid container justifyContent={"space-around"}>
-        {medications.map((item) => {
-
-      <TextField
-        id="standard-search"
-        label="Search field"
-        type="search"
-        onChange={some}
-      />
-
-
-      <Grid container justifyContent={"space-around"}>
-
-        {medications
-        .filter(item => search === "" ? item : item.name.toLowerCase().includes(search))
-        .map((item) => {
-
-          return (
-            <Grid item xs={3} key={item._id}>
-              <Card>
-                <CardActionArea>
-                  <CardMedia
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    title="Contemplative Reptile"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {item.name}
-                    </Typography>
-
-                    <Typography
-                      variant="h6"
-                      color="textSecondary"
-                      component="p"
-                    >
-
-                    <Typography variant="h6" color="textSecondary" component="p">
-
-                      {item.price ? item.price : "Бесплатно"}
-                    </Typography>
-                    <Typography
-                      variant="body1"
-                      color="textSecondary"
-                      component="p"
-                    >
-                      {item.descr}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-                <CardActions>
-                  <Button size="small" color="primary">
-                    <Link to={`/medications/${item._id}`}>Подробнее</Link>
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          );
-        })}
       </Grid>
     </>
   );
