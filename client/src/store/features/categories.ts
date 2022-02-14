b3f826bf09055d2a3a9d7ad29218144c7906dc90
Mansur <vitrigovm@gmail.com>
2022-02-14 08:01:19 +0300

    переписал features categories на toolkit

--- client/src/store/features/categories.ts
+++ client/src/store/features/categories.ts
@@ -1,30 +1,58 @@
-const initialState = {
-  categories: [],
-  loading: false,
-};
+import { SerializedError } from "@reduxjs/toolkit/dist/createAsyncThunk";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-export default function categories(state = initialState, action) {
-  switch (action.type) {
-    case "categories/fetch/fulfilled":
-      return {
-        ...state,
-        loading: false,
-        categories: action.payload,
-      };
-    default:
-      return state;
-  }
+export interface Category {
+  _id?: string;
+  name: string;
 }
 
-export const fetchAllCategories = () => {
-  return async (dispatch) => {
-    try {
-      const response = await fetch("/categories");
-      const json = await response.json();
+interface CategoriesState {
+  categories: Category[];
+  loading: boolean;
+  error: SerializedError | null;
+}
 
-      dispatch({ type: "categories/fetch/fulfilled", payload: json });
-    } catch (e) {
-      dispatch({ type: "categories/fetch/rejected", error: e.toString() });
-    }
-  };
+const initialState: CategoriesState = {
+  categories: [],
+  loading: false,
+  error: null,
 };
+
+export const getCategories = createAsyncThunk(
+  "categories/fetch",
+  async (_, { rejectWithValue }) => {
+    const response = await axios.get("/categories");
+
+    if (!response.data.ok) {
+      rejectWithValue(response.data.error);
+    }
+
+    return response.data;
+  }
+);
+
+const categoriesSlice = createSlice({
+  name: "categories",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getCategories.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(getCategories.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error;
+    });
+    builder.addCase(getCategories.fulfilled, (state, action) => {
+      state.loading = false;
+      state.error = null;
+      state.categories = action.payload;
+    });
+  },
+});
+
+export const {} = categoriesSlice.actions;
+
+export default categoriesSlice.reducer;
