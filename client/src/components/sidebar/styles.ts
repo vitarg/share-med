debe622fea84ad7d066c6449be77a00f338215ce
Adam <bisliev_adam@mail.ru>
2022-03-11 16:28:47 +0300

    Починить активную категорию в Sidebar

--- client/src/components/sidebar/styles.ts
+++ client/src/components/sidebar/styles.ts
@@ -1,14 +1,14 @@
 import { styled } from "@mui/material";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-export const RouterLink = styled(Link)({
-  marginLeft: 16,
-  color: "#6c757d",
-  textDecoration: "none",
-});
+// export const RouterLink = styled(NavLink)({
+//   marginLeft: 16,
+//   color: "#6c757d",
+//   textDecoration: "none",
+// });
 
-export const RouterActiveLink = styled(Link)({
-  marginLeft: 16,
-  color: "#484d52",
-  textDecoration: "none",
-});
+// export const RouterActiveLink = styled(NavLink)({
+//   marginLeft: 16,
+//   color: "#484d52",
+//   textDecoration: "none",
+// });
